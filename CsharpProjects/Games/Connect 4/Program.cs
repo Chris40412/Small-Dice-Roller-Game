7894b7e22bb7e10ba25df1193b9e044b5e78a163
chris40412 <chris40412r@gmail.com>
2025-11-17 19:04:48 -0500

    I was doing all of this while playing a match of tdx endless lol. I set up a show system (that is currently very trash) and I will finish it later.

---
+++ CsharpProjects/Games/Connect 4/Program.cs
@@ -0,0 +1,119 @@
+﻿//Plan: Make 2 player Connect 4 (because idk how I would make a bot)
+//Make the grid 7/6 (Done), pieces (x & o) (Done), add drop effect, both players, then win conditions.
+
+using System;
+using System.Xml;
+
+class Connect4Game
+{
+    static int[,] connect4Board = new int[7, 6];
+
+    static int DropEffect(int collumn) //It will enter which collumn (i, 0), then find the lowest spot in said collumn
+    {
+        bool dropSpotFound = false;
+        int dropSpot = 0;
+        for (int i = 0; i < 6; i++) //Check to see if this is correct later on
+        {
+            switch (connect4Board[collumn, i]) //a this is my making my head hurt trying to visualize
+            {
+                case 0: //Empty spot
+                    {
+                        break;
+                    }
+                case 1 or 2: //Occupied Spot
+                    {
+                        dropSpotFound = true;
+                        dropSpot = i - 1;
+                        break;
+                    }
+                default: { Console.WriteLine("EEEEEEEEERRRRRRRROOOORRRRRR"); break; }
+            }
+            if (dropSpotFound)
+            {
+                break;
+            }
+        }
+        return dropSpot; //I hope this works
+    }
+
+    static int PlayerInput(string instructions, string error, string extra = "")
+    {
+        bool validResponse = false;
+        string? elPlayerInput = "";
+        int integerValue = 0;
+        Console.WriteLine(instructions);
+        do
+        {
+            elPlayerInput = Console.ReadLine();
+            if (int.TryParse(elPlayerInput, out integerValue))
+            {
+                validResponse = true;
+            }
+            else
+            {
+                Console.WriteLine(error);
+            }
+        } while (!validResponse);
+
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    static void Main() //Game start
+    {
+        int gameCondition = 0; //Condition of Game, 0 = ongoing, 1 = p1 win, 2 = p2 win, 3 = draw
+
+
+
+
+        do
+        {
+
+
+
+
+            int i = 0;
+            foreach (int spot in Connect4Game.connect4Board) //Displays board
+            {
+                string piece;
+                if (spot == 0)
+                {
+                    piece = "0";
+                }
+                else if (spot == 1)
+                {
+                    piece = "o";
+                }
+                else if (spot == 2)
+                {
+                    piece = "x";
+                }
+                else
+                {
+                    piece = "error";
+                }
+                Console.Write(piece + "\t");
+                i++;
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        } while (gameCondition == 0);
+    }
+
+
+
+}
+
---
+++ CsharpProjects/Games/Idk/IDK/Program.cs
@@ -0,0 +1,133 @@
+﻿//Idea: make rng game that rolls dice that give you points. Get enough points and you can buy upgrades in a shop that allow you to get more points
+
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
+class IDKgame
+{
+
+    static bool upgrade1 = false, upgrade2 = true, upgrade3 = false, upgrade4 = true;
+    static int rowSelected = 0;
+    static string PlayerInput(string instruction, int type, string error = "")
+    {
+        bool completetion = false;
+        do
+        {
+            Console.WriteLine(instruction);
+            string? input = Console.ReadLine();
+            if (input == null)
+            {
+                input = "";
+            }
+            input = input.Trim().ToLower();
+            if (type == 0) //Shop navigation
+            {
+                if (input == "w" || input == "s" || input == "select")
+                {
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine(error);
+                    continue;
+                }
+            }
+        } while (!completetion);
+
+
+        return "error";
+    }
+
+    static int ShopMovement(int row)
+    {
+        string direction = PlayerInput("Type either 'w' or 's' to move up or down in the shop. Type 'Select' to select an upgrade.", 0);
+        if (direction == "w")
+        {
+            row -= 1;
+            if (row < 0)
+            {
+                row = 0;
+            }
+        }
+        else if (direction == "s")
+        {
+            row += 1;
+            if (row > 3) //change depending on amount of upgrades
+            {
+                row = 3;
+            }
+        }
+        else if (direction == "select")
+        {
+            return row + 100;
+        }
+        return row;
+    }
+
+    static void ShopMenu()
+    {
+        rowSelected = ShopMovement(rowSelected);
+
+        string[] upgradeNames = { "Upgrade One", "Upgrade Two", "Upgrade Three", "Upgrade Four" };
+        for (int i = 0; i < upgradeNames.Length; i++)
+        {
+            string selection = (rowSelected == i) ? ">- " : "";
+            Console.WriteLine($"{selection}{upgradeNames[i]}");
+        }
+
+        if (rowSelected >= 100)
+        {
+            int selectedUpgrade = rowSelected - 100;
+            Console.WriteLine($"Upgrade {upgradeNames[selectedUpgrade]} selected!");
+            if (selectedUpgrade == 0)
+            {
+                upgrade1 = true;
+            }
+            else if (selectedUpgrade == 1)
+            {
+                upgrade2 = true;
+            }
+            else if (selectedUpgrade == 2)
+            {
+                upgrade3 = true;
+            }
+            else if (selectedUpgrade == 3)
+            {
+                upgrade4 = true;
+            }
+        }
+    }
+
+
+    public static void Main()
+    {
+        do
+        {
+            ShopMenu();
+            Thread.Sleep(1000);
+            Console.Clear();
+        } while (true); 
+
+
+        /*
+        int points = 0, diceAmount = 3, diceRoll = 0, diceMin = 1, diceMax = 6;
+        Random dice = new Random();
+        bool gamePlay = true;
+
+        do
+        {
+            diceRoll = 0;
+            for (int i = 0; i < diceAmount; i++)
+            {
+                diceRoll += dice.Next(diceMin, diceMax + 1);
+            }
+            points += diceRoll;
+            Console.WriteLine(points);
+            if (points > 100) { gamePlay = false; }
+        } while (gamePlay);*/
+
+
+
+    }
+
+}
